aa254a8a03e694a1ced063c6377e729d04b9961d
Júnior Messias <jrmessias@gmail.com>
2020-04-03 15:10:44 -0300

    :sparkles: Introducing new features.
    Add bootstrap for ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
